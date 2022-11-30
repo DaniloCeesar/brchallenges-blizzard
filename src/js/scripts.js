59b9fb9efc02b83509b50e6ce15d37b270571a6d
Danilo César <danilocesar@outlook.com>
2022-11-30 15:27:03 -0300

    chore: atualiza importação de assets nos scripts

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -8,25 +8,31 @@ const app = () => {
                 "title": "Diablo IV",
                 "header": "Retorne à escuridão com o game Diablo IV",
                 "description": "O retorno de Lilith traz uma era de escuridão e sofrimento",
-                "badge": "/public/assets/icons-banner-hero/game-1.png",
-                "background": "/public/assets/featured/game-1.png",
-                "trailer": ""
+                "badge": "/public/assets/banner-hero/icons/game-1.png",
+                "background": "/public/assets/banner-hero/games/diablo-bg.png",
+                "logo": "/public/assets/banner-hero/games/diablo-logo.png",
+                "trailer": "/public/assets/banner-hero/games/diablo-animation.gif",
+                "trailer-static": "/public/assets/banner-hero/games/diablo-animation-cover.png"
             },
             {
                 "title": "Hearthstone",
                 "header": "Nova pacote de expansão de Hearthstone",
                 "description": "A Hora e a Aliança se encontraram no Vale Alterac para lutar",
-                "badge": "/public/assets/icons-banner-hero/game-2.png",
-                "background": "/public/assets/featured/game-2.png",
-                "trailer": ""
+                "badge": "/public/assets/banner-hero/icons/game-2.png",
+                "background": "/public/assets/banner-hero/games/hearthstone-bg.png",
+                "logo": "/public/assets/banner-hero/games/hearthstone-logo.png",
+                "trailer": "/public/assets/banner-hero/games/hearthstone-animation.gif",
+                "trailer-static": "/public/assets/banner-hero/games/hearthstone-animation-cover.png"
             },
             {
                 "title": "Shadowlands",
                 "header": "Desbrave as Terras Sombrias em Shadowlands!",
                 "description": "O que jaz além do mundo que você conhece?",
-                "badge": "/public/assets/icons-banner-hero/game-3.png",
-                "background": "/public/assets/featured/game-3.png",
-                "trailer": ""
+                "badge": "/public/assets/banner-hero/icons/game-3.png",
+                "background": "/public/assets/banner-hero/games/wow-bg.png",
+                "logo": "/public/assets/banner-hero/games/wow-logo.png",
+                "trailer": "/public/assets/banner-hero/games/wow-animation.gif",
+                "trailer-static": "/public/assets/banner-hero/games/wow-animation-cover.png"
             }
         ],
 
